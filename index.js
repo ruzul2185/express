a619b4ff1139a5c0033ee83dc7620fd57bbde209
ruzul2185 <ruzulchaure2185@gmail.com>
2025-08-08 16:44:44 +0530

    Comments properly added

--- index.js
+++ index.js
@@ -2,31 +2,40 @@ import Express from "express";
 
 const app = Express();
 
-// Middleware
+// Middleware to parse string to json/obj
 app.use(Express.json());
 
+// Fucntion for get request on route "/"
 const sendData = (request, response) => {
   return response.status(200).json({ message: "Backend is working!!" });
 };
 
-// This is to test git
+// get request
 app.get("/", sendData);
-app.post("/user", (req, res) => {
-  const { email, password } = req.body;
-  // undefined = false
-  //  ! = opposite
-  // !false = true
-  const count = 1;
 
+//  Post request
+app.post("/user", (req, res) => {
+
+  // Checking if the body is empty or not
+  if(!req.body.hasOwnProperty("email") && !req.body.hasOwnProperty("password")){
+    return res.status(400).json({message: "Developers mistake"})
+  } 
+
+  // Destructuring email and password
+  const { email, password } = req.body;
+
+  // Checking if both are present
   if (!email || !password) {
     return res
       .status(406)
       .json({ status: "failed", error: "Email and Password are required!" });
   }
 
+  // If all cases are passed, give a positive response
   return res.status(200).json({ message: "user is authenticated" });
 });
 
+// Listener to listen to incoming requests
 app.listen(5000, () => {
   console.log("Server is running on port 5000");
 });
